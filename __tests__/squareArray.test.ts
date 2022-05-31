66d1d609b3f8f50dedce11fe1113d08916f2a293
Matheus Pimenta <matheuspiment@hotmail.com>
2022-05-31 19:13:57 -0300

    test: fix typo

--- __tests__/squareArray.test.ts
+++ __tests__/squareArray.test.ts
@@ -2,12 +2,12 @@ import squareArray from '../src/squareArray';
 
 describe('squareArray', () => {
 	test('should return the squared values in asc order', () => {
-		// Asc ordered array of positive intergers
+		// Asc ordered array of positive integers
 		expect(squareArray([1, 2, 3, 5, 6, 8, 9])).toStrictEqual([
 			1, 4, 9, 25, 36, 64, 81,
 		]);
 
-		// As ordered array of negative intergers
+		// As ordered array of negative integers
 		expect(squareArray([-2, -1])).toStrictEqual([1, 4]);
 
 		// Sample with positive and negative integers
